ef93409e7de35d5f98a0c9cee6a4aa0fdfd4f2b3
sooj36 <sooj2023@gmail.com>
2023-08-09 15:33:13 +0900

    [FEAT] : ⚙️ 로그인, 회원가입 버튼 extra 로 id 넘김

--- app/src/main/java/com/example/self_introduction/HomeActivity.kt
+++ app/src/main/java/com/example/self_introduction/HomeActivity.kt
@@ -2,10 +2,16 @@ package com.example.self_introduction
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val btn = findViewById<Button>(R.id.login)
+        btn.setOnClickListener {
+            finish()
+        }
     }
 }
--- app/src/main/java/com/example/self_introduction/SignInActivity.kt
+++ app/src/main/java/com/example/self_introduction/SignInActivity.kt
@@ -1,11 +1,26 @@
 package com.example.self_introduction
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        val btn2 = findViewById<Button>(R.id.login) //로그인
+        btn2.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+        val btn = findViewById<Button>(R.id.signup) // 회원가입
+        btn.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+
+        }
     }
 }
