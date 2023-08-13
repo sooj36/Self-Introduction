4fe2984c47b4ef90426d236f71881cd3741ba223
sooj36 <sooj2023@gmail.com>
2023-08-13 23:00:55 +0900

    [FEAT] : ⚙️ SignUpActivity.kt 버튼 활성화 외 수정

--- app/src/main/java/com/example/self_introduction/SignUpActivity.kt
+++ app/src/main/java/com/example/self_introduction/SignUpActivity.kt
@@ -4,9 +4,11 @@ import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import android.text.TextWatcher
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 
 class SignUpActivity : AppCompatActivity() {
@@ -15,31 +17,67 @@ class SignUpActivity : AppCompatActivity() {
     private val id_et : EditText by lazy { findViewById(R.id.id_et) }
     private val pw_et : EditText by lazy { findViewById(R.id.pw_et) }
 
-    private val signup_button : EditText by lazy { findViewById(R.id.signup_button) }
+    private val signup_button : Button by lazy { findViewById(R.id.signup_button) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
-        val nameInputComplete = false
-        val idInputComplete = false
-        val pwInputComplete = false
+        val name_tv = findViewById<TextView>(R.id.name_tv)
+        val id_tv = findViewById<TextView>(R.id.id_tv)
+        val pw_tv = findViewById<TextView>(R.id.pw_tv)
 
-        name_et.addTextChangedListener(object : TextWatcher) {
-        }
+        var nameInputComplete = false
+        var idInputComplete = false
+        var pwInputComplete = false
 
-        signup_button.setOnClickListener {
-            updateSignUpButton()
-        }
+        name_et.addTextChangedListener (object : TextWatcher { //이름
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s != null) {
+                    nameInputComplete = s.length > 1
+                }
+                updateSignUpButton()
+            }
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        name_et.addTextChangedListener (object : TextWatcher { //아이디
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s != null) {
+                    idInputComplete = s.length > 1
+                }
+                updateSignUpButton()
+
+            }
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+
+        name_et.addTextChangedListener (object : TextWatcher { //비밀번호
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int){}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s != null) {
+                    pwInputComplete = s.length > 1
+                }
+                updateSignUpButton()
+            }
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
 
         val btn3 = findViewById<Button>(R.id.signup_button) //회원가입 버튼 실행
         btn3.setOnClickListener {
-            val intent = Intent ("this", SignUpActivity::class.java)
+            val intent = Intent (this, SignUpActivity::class.java)
             startActivity(intent)
         }
 
 
         }
+
+
     fun updateSignUpButton() {
         val InputName = name_et.text.toString()
         val InputId = id_et.text.toString()
@@ -48,12 +86,12 @@ class SignUpActivity : AppCompatActivity() {
         if (InputName.isNotEmpty() && InputId.isNotEmpty() && InputPw.isNotEmpty()) {
             signup_button.isEnabled = true
             signup_button.setBackgroundColor(Color.BLUE)
-            Toast.makeText("this", "회원가입 성공 !", Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "회원가입 성공 !", Toast.LENGTH_LONG).show()
         }
         else {
             signup_button.isEnabled = false
-            signup_button.setBackgroundColor(Color.GRAY)
-            Toast.makeText("this", "입력되지 않은 정보가 있습니다", Toast.LENGTH_LONG).show()
+//            signup_button.setBackgroundColor(Color.GRAY)
+            Toast.makeText(this, "입력되지 않은 정보가 있습니다", Toast.LENGTH_LONG).show()
         }
     }
     }
